6511b52f47ac8d1cfb754e31bc5c100fcf64baa2
yht0827 <yangheetae0827@naver.com>
2023-11-14 15:24:09 +0900

    init commit

---
+++ src/main/java/com/example/indexexample/IndexExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.indexexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndexExampleApplication.class, args);
+    }
+
+}
